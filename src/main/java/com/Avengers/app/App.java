dcdb523ec00ee9f3aaf1a77b510e20fc492ee49c
sniralfa <snir.alfandari@gmail.com>
2017-11-26 15:35:24 +0200

    yaldon

--- src/main/java/com/Avengers/app/App.java
+++ src/main/java/com/Avengers/app/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!2 PAINAR CYNAR ddMAGDALENCHUK poznan" );
+        System.out.println( "Hello World!2 PAINAR CYNAR dFFFdMAGDALENCHUK poznan" );
+
     }
 }
