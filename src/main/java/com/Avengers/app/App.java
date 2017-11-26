4b7d2c1d0e2d528bb955916f7e93830b49b1a97c
liorippen <lior.ippen7@gmail.com>
2017-11-26 13:58:09 +0200

    poznan

--- src/main/java/com/Avengers/app/App.java
+++ src/main/java/com/Avengers/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!2 PAINAR CYNAR ddMAGDALENCHUK" );
+        System.out.println( "Hello World!2 PAINAR CYNAR ddMAGDALENCHUK poznan" );
     }
 }
